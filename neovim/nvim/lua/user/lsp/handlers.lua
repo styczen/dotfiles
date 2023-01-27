24098c0ccff3825fec31fc3556aa1692a807d8d6
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2023-01-27 21:56:28 +0100

    Change deprecated API

--- neovim/nvim/lua/user/lsp/handlers.lua
+++ neovim/nvim/lua/user/lsp/handlers.lua
@@ -49,7 +49,7 @@ local function lsp_keymaps(bufnr)
   -- keymap(bufnr, "n", "gl", '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ border = "rounded" })<CR>', opts)
   keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
   keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.format()' ]]
 end
 
 M.on_attach = function(client, bufnr)
