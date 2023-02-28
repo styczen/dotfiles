eaf2a8a2d83799b911f2914fd0e726a7d6813320
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2023-02-28 19:56:37 +0100

    Fixing moving and indenting lines

--- neovim/nvim/lua/bartek/keymaps.lua
+++ neovim/nvim/lua/bartek/keymaps.lua
@@ -4,7 +4,6 @@ local term_opts = { silent = true }
 local keymap = vim.keymap.set
 
 -- Remap space as leader key
--- keymap("", "<space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -15,13 +14,6 @@ vim.g.maplocalleader = " "
 --   visual_block_mode = "x",
 --   term_mode = "t",
 --   command_mode = "c",
-
--- Normal --
--- Better window navigation (do not use them now)
---keymap("n", "<C-h>", "<C-w>h", opts)
---keymap("n", "<C-j>", "<C-w>j", opts)
---keymap("n", "<C-k>", "<C-w>k", opts)
---keymap("n", "<C-l>", "<C-w>l", opts)
 
 keymap("n", "<leader>e", vim.cmd.Explore)
 keymap("n", "<leader>w", vim.cmd.write)
@@ -46,18 +38,11 @@ keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "<S-j>", ":move '>+1<CR>gv=gv", opts)
+keymap("v", "<S-k>", ":move '<-2<CR>gv=gv", opts)
 
 -- Keep in register value which was originaly copied
 keymap("v", "p", '"_dp', opts)
-
--- Visual Block --
--- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
 -- Terminal --
 -- Better terminal navigation
