0298737876f7fb13f4a5d1e9f15f84a57dcce550
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2023-02-28 11:59:02 +0100

    Fix typo in treesitter configuraion

--- neovim/nvim/after/plugin/treesitter.lua
+++ neovim/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 local ok, configs = pcall(require, 'nvim-treesitter.configs')
 if not ok then
-    vim.notify('"nvim-treesitter.configs" not found', vim.log.level.WARN)
+    vim.notify('"nvim-treesitter.configs" not found', vim.log.levels.WARN)
     return
 end
 
