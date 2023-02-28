7f91def992d68758aa8ef913586ff1d5186a7bf3
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2023-02-28 20:33:56 +0100

    Please dont hate me for creating a ton of small commits; learning more vim

--- neovim/nvim/lua/bartek/keymaps.lua
+++ neovim/nvim/lua/bartek/keymaps.lua
@@ -1,8 +1,6 @@
 local opts = { noremap = true, silent = true }
 local term_opts = { silent = true }
 
-local keymap = vim.keymap.set
-
 -- Remap space as leader key
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
@@ -15,42 +13,45 @@ vim.g.maplocalleader = " "
 --   term_mode = "t",
 --   command_mode = "c",
 
-keymap("n", "<leader>e", vim.cmd.Explore)
-keymap("n", "<leader>w", vim.cmd.write)
+vim.keymap.set("n", "<leader>e", vim.cmd.Explore)
+vim.keymap.set("n", "<leader>w", vim.cmd.write)
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize +2<CR>", opts)
-keymap("n", "<C-Down>", ":resize -2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+vim.keymap.set("n", "<C-Up>", ":resize +2<CR>", opts)
+vim.keymap.set("n", "<C-Down>", ":resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", vim.cmd.bnext, opts)
-keymap("n", "<S-h>", vim.cmd.bprevious, opts)
+vim.keymap.set("n", "<S-l>", vim.cmd.bnext, opts)
+vim.keymap.set("n", "<S-h>", vim.cmd.bprevious, opts)
 
 -- Insert --
-keymap("i", "jk", "<ESC>", opts)
-keymap("i", "kj", "<ESC>", opts)
+vim.keymap.set("i", "jk", "<ESC>", opts)
+vim.keymap.set("i", "kj", "<ESC>", opts)
 
 -- Visual --
 -- Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
 
 -- Move text up and down and apply indents
-keymap("v", "<S-j>", ":move '>+1<CR>gv=gv", opts)
-keymap("v", "<S-k>", ":move '<-2<CR>gv=gv", opts)
+vim.keymap.set("v", "<S-j>", ":move '>+1<CR>gv=gv", opts)
+vim.keymap.set("v", "<S-k>", ":move '<-2<CR>gv=gv", opts)
 
 -- Keep in register value which was originaly copied
-keymap("x", "<leader>p", "\"_dP")
--- keymap("v", "p", '"_dp', opts)
+vim.keymap.set("x", "<leader>p", "\"_dP")
+-- vim.keymap.set("v", "p", '"_dp', opts)
 
 -- Terminal --
 -- Better terminal navigation
-keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
-keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
-keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
-keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+vim.keymap.set("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+vim.keymap.set("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+vim.keymap.set("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+vim.keymap.set("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
 -- Close quickfix window (currently used only when displaying references)
-keymap("n", "<leader>cc", ":cclose<CR>", opts)
+vim.keymap.set("n", "<leader>cc", ":cclose<CR>", opts)
+
+-- Basically black magic - replace the word the cursor is over
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
