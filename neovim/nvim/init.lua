68776f18f87da97b9ae8afe2cd5faa63b98dc50c
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2025-05-11 19:43:12 +0200

    Make LSP work when using new Mason

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -462,28 +462,7 @@ require("lazy").setup({
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       -- Enable the following language servers
-      --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-      --
-      --  Add any additional override configuration in the following tables. Available keys are:
-      --  - cmd (table): Override the default command used to start the server
-      --  - filetypes (table): Override the default list of associated filetypes for the server
-      --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-      --  - settings (table): Override the default settings passed when initializing the server.
-      --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
-
         -- Rust
         rust_analyzer = {},
 
@@ -492,40 +471,40 @@ require("lazy").setup({
 
         -- Python
         pylsp = {
-          -- settings = {
-          --   pylsp = {
-          --     configurationSources = { "flake8" },
-          --     plugins = {
-          --       -- Formatter
-          --       black = {
-          --         enabled = true,
-          --       },
-          --       yapf = {
-          --         enabled = false,
-          --       },
-          --       autopep8 = {
-          --         enabled = false,
-          --       },
-          --       -- Linter
-          --       flake8 = {
-          --         enabled = true,
-          --         maxLineLength = 20,
-          --         extendIgnore = {
-          --           "E203",
-          --         },
-          --       },
-          --       mccabe = {
-          --         enabled = false,
-          --       },
-          --       pycodestyle = {
-          --         enabled = false,
-          --       },
-          --       pyflakes = {
-          --         enabled = false,
-          --       },
-          --     },
-          --   },
-          -- },
+          settings = {
+            pylsp = {
+              configurationSources = { "flake8" },
+              plugins = {
+                -- Formatter
+                black = {
+                  enabled = true,
+                },
+                yapf = {
+                  enabled = false,
+                },
+                autopep8 = {
+                  enabled = false,
+                },
+                -- Linter
+                flake8 = {
+                  enabled = true,
+                  maxLineLength = 88,
+                  extendIgnore = {
+                    "E203",
+                  },
+                },
+                mccabe = {
+                  enabled = false,
+                },
+                pycodestyle = {
+                  enabled = false,
+                },
+                pyflakes = {
+                  enabled = false,
+                },
+              },
+            },
+          },
         },
 
         -- Lua
@@ -536,7 +515,6 @@ require("lazy").setup({
               telemetry = { enable = false },
               diagnostics = {
                 globals = { "vim" },
-                -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
                 disable = { "missing-fields" },
               },
               completion = {
@@ -551,13 +529,15 @@ require("lazy").setup({
 
         -- LaTeX
         texlab = {
-          texlab = {
-            build = {
-              onSave = false,
-            },
-            latexFormatter = "latexindent",
-            latexindent = {
-              modifyLineBreaks = true,
+          settings = {
+            texlab = {
+              build = {
+                onSave = false,
+              },
+              latexFormatter = "latexindent",
+              latexindent = {
+                modifyLineBreaks = true,
+              },
             },
           },
         },
@@ -570,21 +550,13 @@ require("lazy").setup({
       })
       require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
-      require("mason-lspconfig").setup({
-        ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-        automatic_installation = false,
-        handlers = {
-          function(server_name)
-            vim.notify("helllo")
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-            require("lspconfig")[server_name].setup(server)
-          end,
-        },
-      })
+      for server, config in pairs(servers) do
+        config.capabilities = vim.tbl_deep_extend("force", {}, capabilities, config.capabilities or {})
+        vim.lsp.config(server, config)
+      end
+
+      require("mason").setup()
+      require("mason-lspconfig").setup()
     end,
   },
 
@@ -773,18 +745,10 @@ require("lazy").setup({
       },
       indent = { enable = true, disable = { "ruby" } },
     },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
 
 }, {
   ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
     icons = {},
   },
 })
