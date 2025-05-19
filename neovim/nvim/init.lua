2e2a8504ef8dba0cbee1ed10017e40a913faaf2a
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2025-05-19 10:45:52 +0200

    Cleaner NERD fonts installation

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -675,18 +675,6 @@ require("lazy").setup({
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require("mini.surround").setup()
-
-      -- -- Simple and easy statusline.
-      -- local statusline = require("mini.statusline")
-      -- statusline.setup({ use_icons = vim.g.have_nerd_font })
-      --
-      -- -- ---@diagnostic disable-next-line: duplicate-set-field
-      -- -- statusline.section_location = function()
-      -- --   return "%2l:%-2v"
-      -- -- end
-
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
 
