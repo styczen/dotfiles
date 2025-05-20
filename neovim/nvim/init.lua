b48e9e9a86397962ac3026a707e2ac5a4a2afe43
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2025-05-20 11:15:15 +0200

    Cleared Neovim configuration; install snapd

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -4,17 +4,10 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- [[ Setting options ]]
--- Make line numbers default
 vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
-
--- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = "a"
-
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
+vim.opt.showmode = true
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
@@ -24,28 +17,16 @@ vim.schedule(function()
   vim.opt.clipboard = "unnamedplus"
 end)
 
--- Enable break indent
 vim.opt.breakindent = true
-
--- Save undo history
 vim.opt.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-
--- Keep signcolumn on by default
 vim.opt.signcolumn = "yes"
-
--- Decrease update time
 vim.opt.updatetime = 250
-
--- Decrease mapped sequence wait time
 vim.opt.timeoutlen = 300
-
--- Configure how new splits should be opened
 vim.opt.splitright = true
 vim.opt.splitbelow = true
+vim.opt.swapfile = false
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
