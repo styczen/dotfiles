ab0adafac5c67ea01fc765f9699b3df97a907adf
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2025-06-06 13:07:51 +0200

    Set copilot model

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -715,14 +715,14 @@ require("lazy").setup({
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     dependencies = {
-      { "github/copilot.vim" },                       -- or zbirenbaum/copilot.lua
-      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" },
     },
-    build = "make tiktoken",                          -- Only on MacOS or Linux
+    build = "make tiktoken",
     opts = {
-      -- See Configuration section for options
+      model = "claude-sonnet-4",
+      chat_autocomplete = false,
     },
-    -- See Commands section for default commands if you want to lazy load on them
   },
 }, {
   ui = {
