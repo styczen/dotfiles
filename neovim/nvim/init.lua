1e4f2a4f0d20fbe37929720ba7384e7f023e714f
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2025-08-05 11:28:48 +0200

    Minor neovim fixes

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -638,9 +638,6 @@ require("lazy").setup({
       local vscode = require("vscode")
       local c = require("vscode.colors").get_colors()
       vscode.setup({
-        -- Enable transparent background
-        transparent = true,
-
         -- Enable italic comment
         italic_comments = true,
 
@@ -664,27 +661,6 @@ require("lazy").setup({
     event = "VimEnter",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = { signs = false },
-  },
-
-  -- Collection of various small independent plugins/modules
-  {
-    "echasnovski/mini.nvim",
-    config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require("mini.ai").setup({ n_lines = 500 })
-
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require("mini.surround").setup()
-    end,
   },
 
   -- Highlight, edit, and navigate code
