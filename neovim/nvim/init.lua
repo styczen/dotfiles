ef842b77a4664858563ccd6607398f3ea8f0585e
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2025-08-11 23:07:06 +0200

    Remove borders around diagnostics

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -435,7 +435,7 @@ require("lazy").setup({
       -- See :help vim.diagnostic.Opts
       vim.diagnostic.config({
         severity_sort = true,
-        float = { border = "rounded", source = "if_many" },
+        float = { source = "if_many" },
         underline = { severity = vim.diagnostic.severity.ERROR },
         signs = vim.g.have_nerd_font and {
           text = {
