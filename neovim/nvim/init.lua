c9016a0d48b00d2bfe2356360ba6d3c450db89a9
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2025-08-11 11:02:17 +0200

    Trying vscode theme again - with small modifications

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -630,11 +630,26 @@ require("lazy").setup({
     end,
   },
 
+  -- {
+  --   "ellisonleao/gruvbox.nvim",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("gruvbox")
+  --   end,
+  -- },
+
   {
-    "ellisonleao/gruvbox.nvim",
+    "Mofiqul/vscode.nvim",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme("gruvbox")
+      local c = require("vscode.colors").get_colors()
+      require("vscode").setup({
+        style = "dark",
+        group_overrides = {
+          NormalFloat = { bg = c.vscContext },
+        }
+      })
+      vim.cmd.colorscheme("vscode")
     end,
   },
 
